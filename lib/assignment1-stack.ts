b97243c3d60c71c6d84df7d98570ab2aad3b63a9
unknown <jiahchengpan@gmail.com>
2025-03-12 14:19:42 +0000

    Connect Lambda function to API Gateway

--- lib/assignment1-stack.ts
+++ lib/assignment1-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
@@ -13,6 +15,28 @@ export class Assignment1Stack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    new cdk.CfnOutput(this, 'TableName', { value: table.tableName });
+    // Create Lambda Functions
+    const getItemsLambda = new lambda.Function(this, 'GetItemsFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'getItems.handler',
+      code: lambda.Code.fromAsset('lambdas'),
+      environment: { TABLE_NAME: table.tableName },
+    });
+
+    table.grantReadData(getItemsLambda);
+
+    // Create the API Gateway
+    const api = new apigateway.RestApi(this, 'ServerlessApi', {
+      restApiName: 'Serverless Service',
+      description: 'This API serves as a CRUD endpoint for DynamoDB.',
+    });
+
+    // Add GET /items/{partition_key} endpoints
+    const itemsResource = api.root.addResource('items');
+    const item = itemsResource.addResource('{partition_key}');
+    item.addMethod('GET', new apigateway.LambdaIntegration(getItemsLambda));
+
+    // Export API Gateway endpoints
+    new cdk.CfnOutput(this, 'ApiEndpoint', { value: api.url! });
   }
 }
